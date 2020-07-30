a605b6f4e80aceb1c8254fc7f7f2da96e8c6b1c4
palmeida7 <pjalmeida7@gmail.com>
2020-07-30 13:07:29 -0400

    edits

--- install.sql
+++ install.sql
@@ -1,8 +1,36 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY, 
+    user_id SERIAL PRIMARY KEY, 
     username VARCHAR(20),
     password VARCHAR(20),
     email VARCHAR(80),
-    img VARCHAR,
-    dateofbirth date,
+    dateofbirth date
+);
+
+CREATE TYPE IMG_TYPE AS ENUM (
+    'profile',
+    'cover',
+    'blog'
+);
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    owner_id INT REFERENCES users(id),
+    img_type  IMG_TYPE,
+    img_url VARCHAR
+);
+CREATE TABLE profile (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR,
+    pro_id INT REFERENCES images(id),
+    cov_id INT REFERENCES images(id),
+    bio VARCHAR(255)
+);
+
+CREATE TABLE blogs (
+  bid SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  body VARCHAR,
+  user_id INT REFERENCES users(id),
+  author VARCHAR REFERENCES users(username),
+  date_created TIMESTAMP,
+  likes INT DEFAULT 0
 );
