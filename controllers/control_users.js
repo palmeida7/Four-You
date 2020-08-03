6c547dcd79d4aea948c4a7f595aa522e1b464d83
Eric Yim <eric.tk.yim@gmail.com>
2020-08-03 09:28:23 -0400

    create user/profile

---
+++ controllers/control_users.js
@@ -0,0 +1,67 @@
+const db = require('./../../config');
+const img = require('./control_images');
+const bcrypt = require('bcrypt');
+
+let user = {};
+
+user.updateProfile = async function (id, fullName, proImg, covImg, bio){
+    let getProImg = `
+        SELECT * FROM images
+        WHERE img_url = '${proImg}'
+    `;
+    let proImgObj = await db.one(getProImg);
+
+    let getCovImg = `
+        SELECT * FROM images
+        WHERE img_url = '${covImg}'
+    `;
+    let covImgObj = await db.one(getCovImg);
+
+    let proImgId, covImgId;
+    if (proImgObj == null){
+        proImgId = await img.uploadImg(proImg);
+    } else {
+        proImgId = proImgObj.id;
+    };
+
+    if (covImgObj == null){
+        covImgId = await img.uploadImg(covImg);
+    } else {
+        covImgId = covImgObj.id;
+    };
+
+    let userSql = `
+        UPDATE users 
+        SET 
+        full_name = '${fullName}',
+        pro_id = '${proImgId}',
+        cov_id = '${coImgId}',
+        bio = '${bio}'
+        WHERE id = '${id}' RETURNING *
+    `;
+    let userObj = await db.one(userSql);
+    return userObj;
+};
+
+user.createUser = async function (username, password, email) {
+    let existUserSql = `
+        SELECT id FROM users
+        WHERE username = '${username}'
+    `;
+    let existUserObj = await db.one(existUserSql);
+    let encPass = await bcrypt.hash(password, 10);
+    if (existUseObj == null){
+        let newUserSql = `
+        INSERT INTO users (username, password, email) 
+        VALUES ('${username}','${encPass}','${email}')
+        RETURNING *
+        `; 
+        let newUserObj = await db.one(newUserSql);
+        return newUserObj;
+    } else {
+        return null;
+    }
+}
+
+
+module.exports = user;
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const controlUser = require('./../controllers/control_users');
+
+let user = {};
+
+user.createUser = function (req, res) {
+    let userName = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    let userStatus =  controlUser.createUser(userName,email,password);
+    let msg = 'User already exists.';
+    if (userStatus == null){
+        res.status(400).send({msg : msg});
+    } else {
+        res.status(200).send({user : userStatus});
+    }
+};
+
+user.updateUser = function (req, res) {
+    let id = req.body.id;
+    let fullName = req.body.full_name;
+    let proImg = req.body.pro_upload;
+    let covImg = req.body.cover_upload;
+    let bio = req.body.bio;
+    let profileStatus = controlUser.createUser(id,fullName,proImg,covImg,bio);
+    res.status(200).send({user : profileStatus});
+};
+
+module.exports = user;
